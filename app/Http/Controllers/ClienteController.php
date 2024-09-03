ca6f8de17801f5e04acbfbec8db4bb4ccec71586
NubiaY <nubia.yaya@gmail.com>
2024-09-02 21:20:50 -0500

    1ra_Parte_Metodos

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -7,4 +7,47 @@ use Illuminate\Http\Request;
 class ClienteController extends Controller
 {
     //
+     
+    public function GetData(Request $request){
+        $rta= 10+20;
+        return response()-> json ([
+            'status '=> '200',
+            'message'=> 'Data..',
+            'respuesta'=> $rta
+    
+        ]);
+
+    }
+
+
+
+public function save(Request $request){
+    return response()-> json ([
+        'status '=> '200',
+        'message'=> 'guardado con exito'
+
+    ]);
 }
+
+public function update(Request $request){
+    return response()-> json ([
+        'status '=> '200',
+        'message'=> 'cambio con exito'
+
+    ]);
+}
+
+public function Delete(Request $request){
+    return response()-> json ([
+        'status '=> '200',
+        'message'=> 'Elimino con  exito'
+
+    ]);
+}
+
+
+}
+
+
+
+
