a7d7a266f02f7751f948ae664bc4aacd4de23e71
NubiaY <nubia.yaya@gmail.com>
2024-09-05 12:37:00 -0500

    1da_Parte_Metodos

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Htpp\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
     //
      
-    public function GetData(Request $request){
+    public function getData(Request $request){
         $rta= 10+20;
         return response()-> json ([
             'status '=> '200',
-            'message'=> 'Data..',
+            'message'=> 'data..',
             'respuesta'=> $rta
-    
         ]);
 
     }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Htpp\Controller\AuthController;
 use App\Htpp\Controller\ClienteController;
+use App\Htpp\Controller\ProductoBodegaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +21,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/cliente/Getdata', [ClienteController::class, 'GetData']);
+Route::get('/cliente/datos', [ClienteController::class, 'getData']);
+
+Route::POST('/cliente/save', [ClienteController::class, 'save']);
+Route::PUT('/cliente/update', [ClienteController::class, 'update']);
+Route::DELETE('/cliente/Delete', [ClienteController::class, 'Delete']);
+
+
+Route::GET('/ProductoBodega/Getdata', [ProductoBodegaController::class, 'GetData']);
+Route::POST('/ProductoBodega/save', [ProductoBodegaController::class, 'save']);
+Route::PUT('/ProductoBodega/update', [ProductoBodegaController::class, 'update']);
+Route::DELETE('/ProductoBodega/Delete', [ProductoBodegaController::class, 'Delete']);
