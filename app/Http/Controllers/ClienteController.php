4f3f754418ee3fc2cf72de6c0a1af288efab4bcb
NubiaY <nubia.yaya@gmail.com>
2024-09-06 10:34:51 -0500

    Validacion_postman

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -6,45 +6,22 @@ namespace App\Http\Controllers;
 use App\Htpp\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class ClienteController extends Controller
+class ClienteController extends Controller  
 {
     //
      
     public function getData(Request $request){
         $rta= 10+20;
-        return response()-> json ([
-            'status '=> '200',
-            'message'=> 'data..',
-            'respuesta'=> $rta
+        return response()->json([
+            'status' => '200',
+            'message' => 'guardado con exito',
+            'result'=> $rta
         ]);
 
     }
 
 
 
-public function save(Request $request){
-    return response()-> json ([
-        'status '=> '200',
-        'message'=> 'guardado con exito'
-
-    ]);
-}
-
-public function update(Request $request){
-    return response()-> json ([
-        'status '=> '200',
-        'message'=> 'cambio con exito'
-
-    ]);
-}
-
-public function Delete(Request $request){
-    return response()-> json ([
-        'status '=> '200',
-        'message'=> 'Elimino con  exito'
-
-    ]);
-}
 
 
 }
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Htpp\Controller\AuthController;
-use App\Htpp\Controller\ClienteController;
-use App\Htpp\Controller\ProductoBodegaController;
+use App\Htpp\Controllers\AuthController;
+use App\Htpp\Controllers\ClienteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,14 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/cliente/datos', [ClienteController::class, 'getData']);
+Route::get('/clientes/datos/', [ClienteController::class, 'getData']);
 
-Route::POST('/cliente/save', [ClienteController::class, 'save']);
-Route::PUT('/cliente/update', [ClienteController::class, 'update']);
-Route::DELETE('/cliente/Delete', [ClienteController::class, 'Delete']);
-
-
-Route::GET('/ProductoBodega/Getdata', [ProductoBodegaController::class, 'GetData']);
-Route::POST('/ProductoBodega/save', [ProductoBodegaController::class, 'save']);
-Route::PUT('/ProductoBodega/update', [ProductoBodegaController::class, 'update']);
-Route::DELETE('/ProductoBodega/Delete', [ProductoBodegaController::class, 'Delete']);
