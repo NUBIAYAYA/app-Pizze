026e55f4b7d0b7060b6d9c974d1395af03e5d1f8
NubiaY <nubia.yaya@gmail.com>
2024-09-15 20:10:17 -0500

    Final sprint Metodos

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,12 @@ use App\Http\Controllers\DetalleOrdenController;
 use App\Http\Controllers\ProductoBodegaController;
 use App\Http\Controllers\TurnoController;
 use App\Http\Controllers\EmpleadoController;
-/*
+use App\Http\Controllers\PedidoController;
+use App\Http\Controllers\DetalleProveedorController;
+use App\Http\Controllers\MedioPagoController;
+use App\Http\Controllers\PrecioController;
+use App\Http\Controllers\ProveedorController;
+/*  
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
@@ -57,3 +62,33 @@ Route::put('/Empleado/Update', [EmpleadoController::class, 'update']);
 Route::delete('/Empleado/delete', [EmpleadoController::class, 'delete']);
 
 
+
+Route::get('/Pedido/datos', [PedidoController::class, 'getData']);
+Route::post('/Pedido/save', [PedidoController::class, 'save']);
+Route::put('/Pedido/Update', [PedidoController::class, 'update']);
+Route::delete('/Pedido/delete', [PedidoController::class, 'delete']);
+
+
+Route::get('/DetalleProveedor/datos', [DetalleProveedorController::class, 'getData']);
+Route::post('/DetalleProveedor/save', [DetalleProveedorController::class, 'save']);
+Route::put('/DetalleProveedor/Update', [DetalleProveedorController::class, 'update']);
+Route::delete('/DetalleProveedor/delete', [DetalleProveedorController::class, 'delete']);
+
+
+
+Route::get('/MedioPago/datos', [MedioPagoController::class, 'getData']);
+Route::post('/MedioPago/save', [MedioPagoController::class, 'save']);
+Route::put('/MedioPago/Update', [MedioPagoController::class, 'update']);
+Route::delete('/MedioPago/delete', [MedioPagoController::class, 'delete']);
+
+
+
+Route::get('/Precio/datos', [PrecioController::class, 'getData']);
+Route::post('/Precio/save', [PrecioController::class, 'save']);
+Route::put('/Precio/Update', [PrecioController::class, 'update']);
+Route::delete('/Precio/delete', [PrecioController::class, 'delete']);
+
+Route::get('/Proveedor/datos', [ProveedorController::class, 'getData']);
+Route::post('/Proveedor/save', [ProveedorController::class, 'save']);
+Route::put('/Proveedor/Update', [ProveedorController::class, 'update']);
+Route::delete('/Proveedor/delete', [ProveedorController::class, 'delete']);
