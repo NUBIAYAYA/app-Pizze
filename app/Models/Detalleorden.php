c9999226ce720ac1142b7a508272064c5f0bfe45
NubiaY <nubia.yaya@gmail.com>
2024-10-04 13:14:23 -0500

    Metodos

--- app/Models/Detalleorden.php
+++ app/Models/Detalleorden.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Detalle_orden extends Model
+class Detalleorden extends Model
 {
     use HasFactory;
 }
--- app/Models/ProductoBodega.php
+++ app/Models/ProductoBodega.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Producto_Bodega extends Model
+class ProductoBodega extends Model
 {
     use HasFactory;
 }
