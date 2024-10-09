2346f3233a4b3cdb5376b49ecd3d82d7d26123d2
NubiaY <nubia.yaya@gmail.com>
2024-10-08 22:17:18 -0500

    Metho_Provvedor

--- app/Http/Controllers/ProveedorController.php
+++ app/Http/Controllers/ProveedorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Proveedor;
 
 class ProveedorController extends Controller
 {
@@ -20,14 +21,34 @@ class ProveedorController extends Controller
     ]);
 }
 public function save (Request $request){
+
+
+    try{ 
+
+       $proveedor= new Proveedor();
+       $proveedor->nombre_Proveedor= $request->nombre_Proveedor;
+       $proveedor->Cantidad= $request->Cantidad;
+       $proveedor->Valor_Proveedor= $request->Valor_Proveedor;
+       $proveedor->Numero_pedido= $Numero_pedido;
+       $proveedor->Direccion= $request->Direccion;
+       $proveedor->Telefono_Fijo= $request->Telefono_Fijo;
+       $proveedor->N_Celular= $request->N_Celular;
+       $proveedor->Correo_Electronico= $request->Correo_Electronico;
+       $proveedor->Cuidad= $request->Cuidad;
+       $proveedor->id_Producto= $request->id_Productor;
+       $proveedor->save();
+
     return response()->json([
         'status' => '200',
         'message' => 'guardado con exito',
-        'data' => $request -> nombre,
+        'data' => $request -> $proveedor,
     ]);
+
 }
-
-
+catch(\Exception $e){
+    echo $e->getMessage();
+}
+}
 public function update (Request $request){
     return response()->json([
         'status' => '200',
--- app/Models/Proveedor.php
+++ app/Models/Proveedor.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Proveedor extends Model
 {
     use HasFactory;
+    protected $table = 'Proveedors';
+    protected $fillable=['nombre_Proveedor','Cantidad','Valor_Proveedor','Numero_pedido', 'Direccion', 'Telefono_Fijo','N_Celular','Correo_Electronico','Cuidad','id_Producto'];
 }
